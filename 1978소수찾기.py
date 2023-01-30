bfedbebd48be194fcd91b9e418c23a007dcab9cb
JihoPark <hufs71@naver.com>
2023-01-31 01:51:43 +0900

    소수 찾기

---
+++ 1978소수찾기.py
@@ -0,0 +1,18 @@
+n = int(input())
+lista = list(map(int, input().split()))
+
+def so(n):   #소수확인
+    ans=0
+    if n==1:
+        return 1
+    else:
+        for i in range(n):
+            for j in range(n):
+                if i*j==n:
+                    ans+=1
+        return ans
+count=0
+for i in range(n):
+    if so(lista[i])==0:
+        count+=1
+print(count)
