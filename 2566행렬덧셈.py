0fc9cb0ae9edd58992033a92c2535c085e721bc5
JihoPark <hufs71@naver.com>
2023-02-10 21:27:55 +0900

    add

--- 2566행렬덧셈.py
+++ 2566행렬덧셈.py
@@ -1,15 +1,3 @@
-import itertools
-row, max=[],[]
-
-for i in range(9):
-    a=list(map(int,input().split()))
-    row.append(a)
-    sort_a=sorted(a, reverse=True)   #행 숫자큰순서대로
-    max.append(sort_a[0])  #각 행 최대값들
-all=list(itertools.chain(*row))
-max.sort(reverse=True)
-ans=all.index(max[0])     #max[0]: 최댓값
-r=ans//9+1
-c=ans%9+1
-print(max[0])
-print(r,c)
+n=int(input())
+a=list(map(int,input().split()))
+seq=[0]
